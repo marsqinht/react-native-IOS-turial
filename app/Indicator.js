c99d98ee76ed7b32f6c99b25eacdd94ca183e273
marsqinht <qinhaitaocn@163.com>
2017-03-13 21:10:34 +0800

    组件

--- app/Indicator.js
+++ app/Indicator.js
@@ -1,52 +1,53 @@
 'use strict';
-
 import React, { Component } from 'react';
 import { ActivityIndicator, StyleSheet, View } from 'react-native';
-
 /**
  * Optional Flowtype state and timer types definition
  */
+
+
 type State = { animating: boolean; };
 type Timer = number;
 class ToggleAnimatingActivityIndicator extends Component {
-  /**
-   * Optional Flowtype state and timer types
-   */
-  state: State;
-  _timer: Timer;
+    /**
+     * Optional Flowtype state and timer types
+     */
+    state: State;
+    _timer: Timer;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      animating: true,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            animating: true,
+        };
+    }
 
-  componentDidMount() {
-    this.setToggleTimeout();
-  }
+    componentDidMount() {
+        this.setToggleTimeout();
+    }
 
-  componentWillUnmount() {
-    clearTimeout(this._timer);
-  }
+    componentWillUnmount() {
+        clearTimeout(this._timer);
+    }
 
-  setToggleTimeout() {
-    //console.log(this)
-    this._timer = setTimeout(() => {
-      this.setState({animating: !this.state.animating});
-      this.setToggleTimeout();
-    }, 2000);
-  }
-
-  render() {
-    return (
-      <ActivityIndicator
-        animating={this.state.animating}
-        style={[styles.centering, {height: 80}]}
-        size="large"
-      />
-    );
-  }
+    setToggleTimeout() {
+        //console.log(this)
+        this._timer = setTimeout(() => {
+            this.setState({ animating: !this.state.animating });
+            this.setToggleTimeout();
+        }, 2000);
+    }
+    
+    render() {
+        return ( <
+            ActivityIndicator animating = { this.state.animating }
+            style = {
+                [styles.centering, { height: 80 }]
+            }
+            size = "large" /
+            >
+        );
+    }
 }
 
 export default ToggleAnimatingActivityIndicator
@@ -165,17 +166,17 @@ export default ToggleAnimatingActivityIndicator
 
 
 const styles = StyleSheet.create({
-  centering: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 8,
-  },
-  gray: {
-    backgroundColor: '#cccccc',
-  },
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 8,
-  },
+    centering: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+    },
+    gray: {
+        backgroundColor: '#cccccc',
+    },
+    horizontal: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 8,
+    },
 });
